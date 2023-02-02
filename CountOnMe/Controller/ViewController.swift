b23f19b876f10862da8ee7e29f0fe0768d019ed1
Dalbera <gabrielledalbera@icloud.com>
2023-02-02 10:18:37 +0100

    Move properties from Controller to Model

--- CountOnMe/Controller/ViewController.swift
+++ CountOnMe/Controller/ViewController.swift
@@ -12,6 +12,7 @@ class ViewController: UIViewController {
     
     // MARK: - Outlets
     
+    
     @IBOutlet private weak var textView: UITextView!
     @IBOutlet private var numberButtons: [UIButton]!
     @IBOutlet private var operatorButtons: [UIButton]!
@@ -21,114 +22,113 @@ class ViewController: UIViewController {
     
     private var calculator = Calculator()
     
-    private var elements: [String] {
-        return textView.text.split(separator: " ").map { "\($0)" } // Return the text typed in textView and removing spaces
-    }
+    // TODO: when textView.text is updated, send the text value to the text property in Calculator
     
-    private var expressionIsCorrect: Bool { // TODO: - the "-" should be usable first.
-        return elements.last != "+" && elements.last != "-" && elements.last != "x" && elements.last != "/" // Check that the elements selected before = are numbers and not +, -, * or /
-    }
+    // UITextViewDelegate -> textViewDidChange
     
-    private var expressionHaveEnoughElement: Bool {
-        return elements.count >= calculator.minSelection // Check that at least 3 elements have been added to elements (because we need to use at least 2 numbers in operation and a mathematical symbol was used in between
-    }
-    
-    private var canAddOperator: Bool {
-        return elements.last != "+" && elements.last != "-" && elements.last != "x" && elements.last != "/" // Check that mathematical symbols + and - can be added, if there is already "+" or "-" at the end it will return false
-    }
-    
-    private var expressionHaveResult: Bool {
-        return textView.text.firstIndex(of: "=") != nil // Check that the first elements typed in textView is not =
-    }
     
     // MARK: - View Life Cycles
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        resetCalculator()
+        calculator.reset()
     }
-    
     
     // MARK: - View Actions
     
     @IBAction private func tappedResetButton(_ sender: UIButton) {
-        resetCalculator()
+        calculator.reset()
     }
-    
     
     @IBAction private func tappedNumberButton(_ sender: UIButton) {
         guard let numberText = sender.title(for: .normal) else { // Get the title of the button selected and save it to numberText
             return
         }
-        if expressionHaveResult {
+        if calculator.expressionHasResult {
             textView.text = "" // If the first element typed in textView is "=", nothing is displayed in textView
         }
         textView.text.append(numberText) // If all conditions above allows it, add numberText to the text in textView and elements
     }
     
-    
     @IBAction private func tappedOperatorButton(_ sender: UIButton) {
         guard let operatorText = sender.title(for: .normal) else {
             return
         }
-        if expressionHaveResult {
-            resetCalculator()
+        if calculator.expressionHasResult {
+            calculator.reset()
         }
-        if canAddOperator {
+        if calculator.expressionIsCorrect {
             textView.text.append(" \(operatorText) ")
+            //            if operatorText == "+" {
+            //                calculator.operation = Operator.addition
+            //            } else if operatorText == "-" {
+            //                calculator.operation = Operator.substraction
+            //            } else if operatorText == "*" {
+            //                calculator.operation = Operator.multiplication
+            //            } else if operatorText == "÷" {
+            //                calculator.operation = Operator.division
+            //            }
+            
+            
         } else {
-            resetCalculator()
-            let alertVC = UIAlertController(title: "Zéro!", message: "Un operateur est déja mis !", preferredStyle: .alert)
+            calculator.reset()
+            let alertVC = UIAlertController(title: "Zéro!", message: "Entrée invalide !", preferredStyle: .alert)
             alertVC.addAction(UIAlertAction(title: "OK", style: .cancel, handler: nil))
             self.present(alertVC, animated: true, completion: nil)
-            
         }
     }
     
-    
     @IBAction private func tappedEqualButton(_ sender: UIButton) {
-        guard expressionIsCorrect else { // allows typing "=" button only if it is not the first element typed in textView
+        guard calculator.expressionIsCorrect else { // allows typing "=" button only if it is not the first element typed in textView
             let alertVC = UIAlertController(title: "Zéro!", message: "Entrez une expression correcte !", preferredStyle: .alert)
             alertVC.addAction(UIAlertAction(title: "OK", style: .cancel, handler: nil))
             return self.present(alertVC, animated: true, completion: nil)
         }
         
-        guard expressionHaveEnoughElement else { // adds another condition to type "=" button : there should be at least 2 numbers and a mathematical symbol in between
+        guard calculator.expressionHasEnoughElements else { // adds another condition to type "=" button : there should be at least 2 numbers and a mathematical symbol in between
             let alertVC = UIAlertController(title: "Zéro!", message: "Démarrez un nouveau calcul !", preferredStyle: .alert)
             alertVC.addAction(UIAlertAction(title: "OK", style: .cancel, handler: nil))
             return self.present(alertVC, animated: true, completion: nil)
         }
+        // ÷
         
         // Create local copy of operations
-        var operationsToReduce = elements // preparing a var to store the result of the operations (need to take the indexes of elements for that)
+        var operationsToReduce = calculator.elements // preparing a var to store the result of the operations (need to take the indexes of elements for that)
         
         // Iterate over operations while an operand still here
         while operationsToReduce.count > 1 { // The strategy here is to keep only one data in operationsToReduce, which will be the result so there is a while loop to continue as long as there is more than 1 index.
-            let left = Int(operationsToReduce[0])! // Index 0 is stored in "left" constant
-            let right = Int(operationsToReduce[2])! // Index 2 is stored in "right" constant
             // If there are more indexes, the first 3 will be calculated, the result takes the first place and the 2 following indexes will be removed and it continues until there is only 1 index in the array.
             
             
+            calculator.transformNegativeIndexes(0)
+            calculator.leftValue = Int(operationsToReduce[0])! // Index 0 is stored in "leftValue" constant
+            calculator.transformNegativeIndexes(2)
+
+            if operationsToReduce[2] == "+" || operationsToReduce[2] == "-" {
+                let newNumber = operationsToReduce[2] + operationsToReduce[3]
+                operationsToReduce[2] = newNumber
+                operationsToReduce.remove(at: 3) // After the fusion we remove the element at the index 1.
+            }
+            calculator.rightValue = Int(operationsToReduce[2])! // Index 2 is stored in "rightValue" constant
+            
             guard let operand = Operator(rawValue: operationsToReduce[1]) else {
-                resetCalculator()
+                calculator.reset()
                 let alertVC = UIAlertController(title: "Zéro!", message: "Entrez une expression correcte !", preferredStyle: .alert)
                 alertVC.addAction(UIAlertAction(title: "OK", style: .cancel, handler: nil))
                 return self.present(alertVC, animated: true, completion: nil)
             }
-            
-            let result = calculator.calculate(left, operand, right)
+            let result = calculator.calculate(operand)
             operationsToReduce = Array(operationsToReduce.dropFirst(3)) // Remove first 3 index of operationsToReduce
             operationsToReduce.insert("\(result)", at: 0) // add the result of the operation to var operationsToReduce at index 0
         }
         
         textView.text.append(" = \(operationsToReduce.first!)") // add in textView the result of the operation (reading index 0 of operationsToReduce)
+        
     }
     
     // MARK: - Methods
+
     
-    private func resetCalculator() {
-        textView.text = ""
-    }
-    
+
 }
 
--- CountOnMe/Model/Calculator.swift
+++ CountOnMe/Model/Calculator.swift
@@ -12,7 +12,8 @@ enum Operator: String {
     case addition = "+"
     case substraction = "-"
     case multiplication = "x"
-    case division = "/"
+    case division = "÷"
+    case none
 }
 
 class Calculator {
@@ -21,17 +22,69 @@ class Calculator {
     // Store the result
     
     var minSelection = 3
-
-    private var result: Int = 0
     
-    func calculate(_ leftValue: Int, _ operation: Operator, _ rightValue: Int) -> Int{        
+    var leftValue: Int = 0
+    var rightValue: Int = 0
+    var operation = Operator.none
+    
+    var text: String = ""
+    
+    var elements: [String] {
+        return text.split(separator: " ").map { "\($0)" } // Return the text typed in textView and removing spaces
+    }
+    
+    var expressionIsCorrect: Bool {
+        return elements.first != "+" && elements.first != "-" && elements.first != "x" && elements.first != "÷" // Check that mathematical symbols + and - can be added, if there is already "+" or "-" at the end it will return false
+    }
+    
+    private var canAddOperator: Bool {
+        return elements.last != "+" && elements.last != "-" && elements.last != "x" && elements.last != "÷" // Check that mathematical symbols + and - can be added, if there is already "+" or "-" at the end it will return false
+    }
+    
+    var expressionHasEnoughElements: Bool {
+        return elements.count >= minSelection // Check that at least 3 elements have been added to elements (because we need to use at least 2 numbers in operation and a mathematical symbol was used in between
+    }
+    
+    var expressionHasResult: Bool {
+        return text.firstIndex(of: "=") != nil // Check that the first elements typed in textView is not =
+    }
+    
+    func transformNegativeIndexes(_ index: Int) {
+        var operationsToReduce = elements
+        if operationsToReduce[index] == "-" {
+            let newNumber = operationsToReduce[index] + operationsToReduce[index + 1]
+            operationsToReduce[index] = newNumber
+            operationsToReduce.remove(at: index + 1) // After the fusion we remove the element at the index + 1.
+        }
+    }
+    
+    
+    func reset() {
+        text = ""
+    }
+    
+   
+//
+//    func clearCalculator() {
+//
+//    }
+    
+    func calculate(_ operand: Operator) -> Int{
         let result: Int
-        switch operation {
+        switch operand {
         case .addition: result = leftValue + rightValue
         case .substraction: result = leftValue - rightValue
         case .multiplication: result = leftValue * rightValue
-        case .division: result = leftValue / rightValue // TODO: - Add conditions, values should not be 0
+        case .division:
+            if (leftValue > 0 && rightValue > 0) {
+                return  leftValue / rightValue
+            } else {
+                return 0
+            }
+        case .none:
+            return 0
         }
         return result
     }
+    
 }
