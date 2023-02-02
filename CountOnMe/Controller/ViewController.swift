4d90839289fb041d521173e259e82cee9201798d
Dalbera <gabrielledalbera@icloud.com>
2023-02-02 10:52:58 +0100

    Transfer all calculation methods to Model

--- CountOnMe/Controller/ViewController.swift
+++ CountOnMe/Controller/ViewController.swift
@@ -8,6 +8,9 @@
 
 import UIKit
 
+// TODO: When user taps "=", we need to get the result from Calculator and display it to the textView
+
+
 class ViewController: UIViewController {
     
     // MARK: - Outlets
@@ -22,11 +25,6 @@ class ViewController: UIViewController {
     
     private var calculator = Calculator()
     
-    // TODO: when textView.text is updated, send the text value to the text property in Calculator
-    
-    // UITextViewDelegate -> textViewDidChange
-    
-    
     // MARK: - View Life Cycles
     
     override func viewDidLoad() {
@@ -38,6 +36,7 @@ class ViewController: UIViewController {
     
     @IBAction private func tappedResetButton(_ sender: UIButton) {
         calculator.reset()
+        textView.text = ""
     }
     
     @IBAction private func tappedNumberButton(_ sender: UIButton) {
@@ -45,9 +44,11 @@ class ViewController: UIViewController {
             return
         }
         if calculator.expressionHasResult {
-            textView.text = "" // If the first element typed in textView is "=", nothing is displayed in textView
+            calculator.reset()
+            textView.text = ""
         }
-        textView.text.append(numberText) // If all conditions above allows it, add numberText to the text in textView and elements
+        textView.text.append(numberText) // If all conditions above allows it, add numberText to the text in textView
+        calculator.text.append(numberText) // when textView.text is updated, we need to send the text value to the text property in Calculator to be able to compute
     }
     
     @IBAction private func tappedOperatorButton(_ sender: UIButton) {
@@ -56,9 +57,11 @@ class ViewController: UIViewController {
         }
         if calculator.expressionHasResult {
             calculator.reset()
+            textView.text = ""
         }
         if calculator.expressionIsCorrect {
             textView.text.append(" \(operatorText) ")
+            calculator.text.append(" \(operatorText) ")
             //            if operatorText == "+" {
             //                calculator.operation = Operator.addition
             //            } else if operatorText == "-" {
@@ -72,6 +75,7 @@ class ViewController: UIViewController {
             
         } else {
             calculator.reset()
+            textView.text = ""
             let alertVC = UIAlertController(title: "Zéro!", message: "Entrée invalide !", preferredStyle: .alert)
             alertVC.addAction(UIAlertAction(title: "OK", style: .cancel, handler: nil))
             self.present(alertVC, animated: true, completion: nil)
@@ -90,40 +94,10 @@ class ViewController: UIViewController {
             alertVC.addAction(UIAlertAction(title: "OK", style: .cancel, handler: nil))
             return self.present(alertVC, animated: true, completion: nil)
         }
-        // ÷
         
-        // Create local copy of operations
-        var operationsToReduce = calculator.elements // preparing a var to store the result of the operations (need to take the indexes of elements for that)
-        
-        // Iterate over operations while an operand still here
-        while operationsToReduce.count > 1 { // The strategy here is to keep only one data in operationsToReduce, which will be the result so there is a while loop to continue as long as there is more than 1 index.
-            // If there are more indexes, the first 3 will be calculated, the result takes the first place and the 2 following indexes will be removed and it continues until there is only 1 index in the array.
-            
-            
-            calculator.transformNegativeIndexes(0)
-            calculator.leftValue = Int(operationsToReduce[0])! // Index 0 is stored in "leftValue" constant
-            calculator.transformNegativeIndexes(2)
-
-            if operationsToReduce[2] == "+" || operationsToReduce[2] == "-" {
-                let newNumber = operationsToReduce[2] + operationsToReduce[3]
-                operationsToReduce[2] = newNumber
-                operationsToReduce.remove(at: 3) // After the fusion we remove the element at the index 1.
-            }
-            calculator.rightValue = Int(operationsToReduce[2])! // Index 2 is stored in "rightValue" constant
-            
-            guard let operand = Operator(rawValue: operationsToReduce[1]) else {
-                calculator.reset()
-                let alertVC = UIAlertController(title: "Zéro!", message: "Entrez une expression correcte !", preferredStyle: .alert)
-                alertVC.addAction(UIAlertAction(title: "OK", style: .cancel, handler: nil))
-                return self.present(alertVC, animated: true, completion: nil)
-            }
-            let result = calculator.calculate(operand)
-            operationsToReduce = Array(operationsToReduce.dropFirst(3)) // Remove first 3 index of operationsToReduce
-            operationsToReduce.insert("\(result)", at: 0) // add the result of the operation to var operationsToReduce at index 0
-        }
-        
-        textView.text.append(" = \(operationsToReduce.first!)") // add in textView the result of the operation (reading index 0 of operationsToReduce)
-        
+        let result = calculator.calculate()
+        textView.text = ""
+        textView.text.append(" = \(result)") // add in textView the result of the operation (reading index 0 of operationsToReduce)
     }
     
     // MARK: - Methods
--- CountOnMe/Model/Calculator.swift
+++ CountOnMe/Model/Calculator.swift
@@ -49,7 +49,7 @@ class Calculator {
         return text.firstIndex(of: "=") != nil // Check that the first elements typed in textView is not =
     }
     
-    func transformNegativeIndexes(_ index: Int) {
+    private func transformNegativeIndexes(_ index: Int) {
         var operationsToReduce = elements
         if operationsToReduce[index] == "-" {
             let newNumber = operationsToReduce[index] + operationsToReduce[index + 1]
@@ -58,33 +58,57 @@ class Calculator {
         }
     }
     
-    
     func reset() {
         text = ""
     }
     
-   
-//
-//    func clearCalculator() {
-//
-//    }
     
-    func calculate(_ operand: Operator) -> Int{
-        let result: Int
-        switch operand {
-        case .addition: result = leftValue + rightValue
-        case .substraction: result = leftValue - rightValue
-        case .multiplication: result = leftValue * rightValue
-        case .division:
-            if (leftValue > 0 && rightValue > 0) {
-                return  leftValue / rightValue
-            } else {
+    //
+    //    func clearCalculator() {
+    //
+    //    }
+    
+    func calculate() -> Int {
+        var result: Int
+        // Create local copy of operations
+        var operationsToReduce = elements
+        // Iterate over operations while an operand still here
+        while operationsToReduce.count > 1 { // The strategy here is to keep only one data in operationsToReduce, which will be the result so there is a while loop to continue as long as there is more than 1 index.
+            // If there are more indexes, the first 3 will be calculated, the result takes the first place and the 2 following indexes will be removed and it continues until there is only 1 index in the array.
+            transformNegativeIndexes(0)
+            leftValue = Int(operationsToReduce[0])! // Index 0 is stored in "leftValue" constant
+            transformNegativeIndexes(2)
+            rightValue = Int(operationsToReduce[2])! // Index 2 is stored in "rightValue" constant
+            
+            guard let operand = Operator(rawValue: operationsToReduce[1]) else {
+                reset() // TODO: Send error to Controller???
+                //                textView.text = ""
+                //                let alertVC = UIAlertController(title: "Zéro!", message: "Entrez une expression correcte !", preferredStyle: .alert)
+                //                alertVC.addAction(UIAlertAction(title: "OK", style: .cancel, handler: nil))
+                //                return self.present(alertVC, animated: true, completion: nil)
                 return 0
             }
-        case .none:
-            return 0
+            
+            switch operand {
+            case .addition: result = leftValue + rightValue
+            case .substraction: result = leftValue - rightValue
+            case .multiplication: result = leftValue * rightValue
+            case .division:
+                if (leftValue > 0 && rightValue > 0) {
+                    return  leftValue / rightValue
+                } else {
+                    return 0
+                }
+            case .none:
+                return 0
+            }
+            operationsToReduce = Array(operationsToReduce.dropFirst(3)) // Remove first 3 index of operationsToReduce
+            operationsToReduce.insert("\(result)", at: 0) // add the result of the operation to var operationsToReduce at index 0
+            return result
         }
-        return result
+        return 0
     }
+    
+    
     
 }
