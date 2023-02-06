31ebea872e7dcd76681455a4324ce7bc70d7af32
Dalbera <gabrielledalbera@icloud.com>
2023-02-06 10:50:28 +0100

    Cleaning code

--- CountOnMe/Controller/ViewController.swift
+++ CountOnMe/Controller/ViewController.swift
@@ -38,7 +38,7 @@ class ViewController: UIViewController {
         guard let numberText = sender.title(for: .normal) else { // Get the title of the button selected and save it to numberText
             return
         }
-        if calculator.expressionHasResult { // TODO: Doesn't work. When textView starts with "=", we need to reset everything if the user types a number
+        if textView.text.first == "=" {
             resetCalculator()
         }
         addToCalculator(numberText)
@@ -48,26 +48,16 @@ class ViewController: UIViewController {
         guard let operatorText = sender.title(for: .normal) else {
             return
         }
-        if calculator.expressionHasResult { // TODO: Doesn't work. When textView starts with "=", we need to reset everything if the user types an operator
+        if textView.text.first == "=" {
             resetCalculator()
         }
-        if textView.text == "" { // If user starts calculation with operator +, x or ÷, then return error
-            if operatorText != "-" {
-                let alertVC = UIAlertController(title: "Zéro!", message: "Le calcul ne peut pas démarrer par +, x ou ÷. Démarrez un nouveau calcul !", preferredStyle: .alert)
-                alertVC.addAction(UIAlertAction(title: "OK", style: .cancel, handler: nil))
-                return self.present(alertVC, animated: true, completion: nil)
-            } else {
-                addToCalculator(" \(operatorText) ")
-            }
-        } else if calculator.previousElementIsNumber { // If user has entered a number before the operator, add operator to calculator
-            addToCalculator(" \(operatorText) ")
-        } else if !calculator.previousElementIsNumber && operatorText == "-" { // If there is already an operator BUT the user tries to add "-", allows it, but don't change the operation type as the minus will make the following number negative
-            addToCalculator(" \(operatorText) ")
-        } else {
+        if textView.text ==  "" || !calculator.canAddOperator { // If user starts calculation with an operator, then return error
             resetCalculator()
-            let alertVC = UIAlertController(title: "Zéro!", message: "Entrée invalide !", preferredStyle: .alert)
+            let alertVC = UIAlertController(title: "Zéro!", message: "Entrée invalide ! Démarrez un nouveau calcul.", preferredStyle: .alert)
             alertVC.addAction(UIAlertAction(title: "OK", style: .cancel, handler: nil))
-            self.present(alertVC, animated: true, completion: nil)
+            return self.present(alertVC, animated: true, completion: nil)
+        } else {
+            addToCalculator(" \(operatorText) ")
         }
     }
     
@@ -81,7 +71,7 @@ class ViewController: UIViewController {
         
         let result = calculator.calculate()
         textView.text = ""
-        textView.text.append("\(result)") // add in textView the result of the operation (reading index 0 of operationsToReduce)
+        textView.text.append("= \(result)") // add in textView the result of the operation (reading index 0 of operationsToReduce)
         
     }
     
@@ -94,7 +84,7 @@ class ViewController: UIViewController {
     
     private func addToCalculator(_ element: String) {
         textView.text.append(element) // Communicate element to the View
-        calculator.text.append(element) // Communicate element to Model
+        calculator.appendText(element) // Communicate element to Model
     }
     
 }
--- CountOnMe/Model/Calculator.swift
+++ CountOnMe/Model/Calculator.swift
@@ -22,24 +22,19 @@ class Calculator {
         case division = "÷"
     }
     
-    var text: String = ""
+    private(set) var text: String = ""
     
     var elements: [String] {
         return text.split(separator: " ").map { "\($0)" } // Return the text typed in textView and removing spaces
     }
     
     var expressionIsCorrect: Bool {
-        return elements.first != "+" && elements.first != "-" && elements.first != "x" && elements.first != "÷" // Check if the first element is anything different than the 4 operators
-    }
-    
-    var previousElementIsNumber: Bool {
-        return elements.last != "+" && elements.last != "-" && elements.last != "x" && elements.last != "÷" // Check if the last entry is anything other than an operator
-    }
+        return elements.first != "+" && elements.first != "-" && elements.first != "x" && elements.first != "÷" && elements.first != "" // Check if the first element is anything different than the 4 operators
+    } // TODO: Doesn't work, for now the logic is in ViewController
     
     var canAddOperator: Bool {
         return elements.last != "+" && elements.last != "-" && elements.last != "x" && elements.last != "÷" // Check if the last entry is anything other than an operator
-        //        return operation == .none
-    }
+    } // TODO: Doesn't work, for now the logic is in ViewController
     
     var expressionHasEnoughElements: Bool {
         return elements.count >= minSelection // Check that at least 3 elements have been added to elements (because we need to use at least 2 numbers in operation and a mathematical symbol was used in between
@@ -47,61 +42,28 @@ class Calculator {
     
     var expressionHasResult: Bool {
         return text.firstIndex(of: "=") != nil // Check that the first elements typed in textView is not =
-    }
+    } // TODO: Doesn't work, for now the logic is in ViewController
     
     func reset() {
         text = ""
     }
-
-//
-//    func calculate() -> Int {
-//        var result: Int
-//        // Create local copy of operations
-//        var operationsToReduce = elements
-//        // Iterate over operations while an operand still here
-//
-//        while operationsToReduce.count > 1 {
-//
-//            let leftValue = Int(operationsToReduce[0])!
-//            let rightValue = Int(operationsToReduce[2])!
-//
-//            guard let operand = Operator(rawValue: operationsToReduce[1]) else {
-//                reset() // TODO: Send error to Controller???
-//                return 0
-//            }
-//
-//            switch operand {
-//            case .addition: result = leftValue + rightValue
-//            case .substraction: result = leftValue - rightValue
-//            case .multiplication: result = leftValue * rightValue
-//            case .division:
-//                if (leftValue > 0 && rightValue > 0) {
-//                    result = leftValue / rightValue
-//                } else {
-//                    result = 0
-//                }
-//            }
-//            operationsToReduce = Array(operationsToReduce.dropFirst(3)) // Remove first 3 index of operationsToReduce
-//            operationsToReduce.insert("\(result)", at: 0) // add the result of the operation to var operationsToReduce at index 0
-//            return result
-//        }
-//        return 0
-//    }
-//
     
+    func appendText(_ message: String) {
+        text.append(message)
+    }
     
-    func calculate() -> Int {
+    func calculate() -> Double {
         // Create local copy of operations
         var operationsToReduce = elements
         // Priority given to multiplication and division
         operationsToReduce.forEach {
             if $0 == "÷" || $0 == "x" {
                 var operatorIndex = operationsToReduce.firstIndex(of: $0)!
-                let leftValue = Int(operationsToReduce[operatorIndex - 1])!
+                let leftValue = Double(operationsToReduce[operatorIndex - 1])!
                 let operand = Operator(rawValue: operationsToReduce[operatorIndex])
-                let rightValue = Int(operationsToReduce[operatorIndex - 1])!
+                let rightValue = Double(operationsToReduce[operatorIndex - 1])!
                 
-                let result: Int
+                let result: Double
                 
                 switch operand {
                 case .multiplication: result = leftValue * rightValue
@@ -141,7 +103,7 @@ class Calculator {
     
         }
         
-        let total = Int(operationsToReduce[0])!
+        let total = Double(operationsToReduce[0])!
         return total
         
     }
